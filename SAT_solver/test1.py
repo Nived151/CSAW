98af74d5e0029996bf5f57f17c05792be0b2c5bd
Nived <67339427+Nived151@users.noreply.github.com>
2022-10-19 13:17:42 -0500

    functioned i/o keys and miter circuit

--- SAT_solver/test1.py
+++ SAT_solver/test1.py
@@ -19,50 +19,53 @@ with open("C:\\Users\\sxc210186\\Documents\\GitHub\\CSAW\\SAT_solver\\netlist.tx
         inp1 = gates[2].partition(",")
         inp2 = inp1[2].partition(")")
         
+        def iokeys():
         # inp, keys, out as list
-        if "INPUT" in part[0]:
-            if("k" in part[0].split("(")[1].split(")")[0]):
-                key.append(part[0].split("(")[1].split(")")[0])
+            if "INPUT" in part[0]:
+                if("k" in part[0].split("(")[1].split(")")[0]):
+                    key.append(part[0].split("(")[1].split(")")[0])
+                    print(part[0].split("(")[1].split(")")[0],"&")
+                else:
+                    inp.append(part[0].split("(")[1].split(")")[0])
+                    print(part[0].split("(")[1].split(")")[0],"&")
+            if "OUTPUT" in part[0]:
+                outp.append(part[0].split("(")[1].split(")")[0])
                 print(part[0].split("(")[1].split(")")[0],"&")
-            else:
-                inp.append(part[0].split("(")[1].split(")")[0])
-                print(part[0].split("(")[1].split(")")[0],"&")
-        if "OUTPUT" in part[0]:
-            outp.append(part[0].split("(")[1].split(")")[0])
-            print(part[0].split("(")[1].split(")")[0],"&")
         
-        
+        def miter_firsthalf():
         # half miter circuit
-        if " nand" == gates[0]:
-            print(part[0]+ "<=> ~("+inp1[0]+" & "+inp2[0]+")&")
-        elif " and" == gates[0]:
-            print(part[0]+ "<=> ~("+inp1[0]+" & "+inp2[0]+")&")
-        elif " xnor" == gates[0]:
-            print(part[0]+ "<=> ~("+inp1[0]+" + "+inp2[0]+")&")
-        elif " xor" == gates[0]:
-            print(part[0]+ "<=> ("+inp1[0]+" + "+inp2[0]+")&")
-        elif " nor" == gates[0]:
-            print(part[0]+ "<=> ~("+inp1[0]+" | "+inp2[0]+")&")
-        elif " or" == gates[0]:
-            print(part[0]+ "<=> ("+inp1[0]," | "+inp2[0]+")&")
-        elif " not" == gates[0]:
-            print(part[0]+"<=> ~("+inp1[0].partition(")")[0]+")&")
+            if " nand" == gates[0]:
+                print(part[0]+ "<=> ~("+inp1[0]+" & "+inp2[0]+")&")
+            elif " and" == gates[0]:
+                print(part[0]+ "<=> ~("+inp1[0]+" & "+inp2[0]+")&")
+            elif " xnor" == gates[0]:
+                print(part[0]+ "<=> ~("+inp1[0]+" + "+inp2[0]+")&")
+            elif " xor" == gates[0]:
+                print(part[0]+ "<=> ("+inp1[0]+" + "+inp2[0]+")&")
+            elif " nor" == gates[0]:
+                print(part[0]+ "<=> ~("+inp1[0]+" | "+inp2[0]+")&")
+            elif " or" == gates[0]:
+                print(part[0]+ "<=> ("+inp1[0]," | "+inp2[0]+")&")
+            elif " not" == gates[0]:
+                print(part[0]+"<=> ~("+inp1[0].partition(")")[0]+")&")
 
+        def miter_secondhalf():
         #other Half miter circuit
-        if " nand" == gates[0]:
-            print(part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" & "+inp2[0]+")&")
-        elif " and" == gates[0]:
-            print(part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" & "+inp2[0]+")&")
-        elif " xnor" == gates[0]:
-            print(part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" + "+inp2[0]+")&")
-        elif " xor" == gates[0]:
-            print(part[0].split()[0]+"'"+ "<=> ("+inp1[0]+" + "+inp2[0]+")&")
-        elif " nor" == gates[0]:
-            print(part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" | "+inp2[0]+")&")
-        elif " or" == gates[0]:
-            print(part[0].split()[0]+"'"+ "<=> ("+inp1[0]," | "+inp2[0]+")&")
-        elif " not" == gates[0]:
-            print(part[0].split()[0]+"'"+"<=> ~("+inp1[0].partition(")")[0]+")&")
+            if " nand" == gates[0]:
+                print(part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" & "+inp2[0]+")&")
+            elif " and" == gates[0]:
+                print(part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" & "+inp2[0]+")&")
+            elif " xnor" == gates[0]:
+                print(part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" + "+inp2[0]+")&")
+            elif " xor" == gates[0]:
+                print(part[0].split()[0]+"'"+ "<=> ("+inp1[0]+" + "+inp2[0]+")&")
+            elif " nor" == gates[0]:
+                print(part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" | "+inp2[0]+")&")
+            elif " or" == gates[0]:
+                print(part[0].split()[0]+"'"+ "<=> ("+inp1[0]," | "+inp2[0]+")&")
+            elif " not" == gates[0]:
+                print(part[0].split()[0]+"'"+"<=> ~("+inp1[0].partition(")")[0]+")&")
+
 
 
         
