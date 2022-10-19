b99176974a888db162576e8cdd93906f74ca4caa
Nived <67339427+Nived151@users.noreply.github.com>
2022-10-19 12:27:52 -0500

    Update SAT_solver.py

--- SAT_solver/test1.py
+++ SAT_solver/test1.py
@@ -1,7 +1,16 @@
+'''convert .bench to .txt file
+in txt file ignore inputs and outputs
+convert the gates to oracle
+add oracle variables to change it into miter circuit
+put the miter circuit to logictools , find an api or something to integrate the logic tools
+depending on the outputs of logic tools, change the variables in miter circuit
+iterate the code unitl no dip
+insert the variables when the dip is false to get keys'''
+
 key=[]
 inp=[]
 outp=[]
-with open("C:\\Users\\sxc210186\\Documents\\GitHub\\CSAW\\SAT_solver\\netlist1.txt", 'r') as f:
+with open("C:\\Users\\sxc210186\\Documents\\GitHub\\CSAW\\SAT_solver\\netlist.txt", 'r') as f:
     lines = f.readlines()
     for line in lines:
         part = line.partition('=')
