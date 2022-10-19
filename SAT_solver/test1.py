b53f9801910a92c0d00c532bc9c1963e3aa1150d
Nived <67339427+Nived151@users.noreply.github.com>
2022-10-19 13:43:27 -0500

    replaced ' with x

--- SAT_solver/test1.py
+++ SAT_solver/test1.py
@@ -52,24 +52,24 @@ with open("C:\\Users\\sxc210186\\Documents\\GitHub\\CSAW\\SAT_solver\\netlist.tx
         def miter_secondhalf():
         #other Half miter circuit
             if " nand" == gates[0]:
-                print("("+part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" & "+inp2[0]+"))&")
+                print("("+part[0].split()[0]+"x"+ "<=> ~("+inp1[0]+" & "+inp2[0]+"))&")
             elif " and" == gates[0]:
-                print("("+part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" & "+inp2[0]+"))&")
+                print("("+part[0].split()[0]+"x"+ "<=> ~("+inp1[0]+" & "+inp2[0]+"))&")
             elif " xnor" == gates[0]:
-                print("("+part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" + "+inp2[0]+"))&")
+                print("("+part[0].split()[0]+"x"+ "<=> ~("+inp1[0]+" + "+inp2[0]+"))&")
             elif " xor" == gates[0]:
-                print("("+part[0].split()[0]+"'"+ "<=> ("+inp1[0]+" + "+inp2[0]+"))&")
+                print("("+part[0].split()[0]+"x"+ "<=> ("+inp1[0]+" + "+inp2[0]+"))&")
             elif " nor" == gates[0]:
-                print("("+part[0].split()[0]+"'"+ "<=> ~("+inp1[0]+" | "+inp2[0]+"))&")
+                print("("+part[0].split()[0]+"x"+ "<=> ~("+inp1[0]+" | "+inp2[0]+"))&")
             elif " or" == gates[0]:
-                print("("+part[0].split()[0]+"'"+ "<=> ("+inp1[0]," | "+inp2[0]+"))&")
+                print("("+part[0].split()[0]+"x"+ "<=> ("+inp1[0]," | "+inp2[0]+"))&")
             elif " not" == gates[0]:
-                print("("+part[0].split()[0]+"'"+"<=> ~("+inp1[0].partition(")")[0]+"))&")
+                print("("+part[0].split()[0]+"x"+"<=> ~("+inp1[0].partition(")")[0]+"))&")
             
             if "INPUT" in part[0]:
                 if("k" in part[0].split("(")[1].split(")")[0]):
                     key.append(part[0].split("(")[1].split(")")[0])
-                    print(part[0].split("(")[1].split(")")[0]+"'","&")
+                    print(part[0].split("(")[1].split(")")[0]+"x","&")
 
         iokeys()
         miter_firsthalf()
