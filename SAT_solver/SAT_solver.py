43d968bdabd0a05902a8ac453ecd6b165a4552ba
Nived <67339427+Nived151@users.noreply.github.com>
2022-10-19 15:15:43 -0500

    Update SAT_solver.py

--- SAT_solver/SAT_solver.py
+++ SAT_solver/SAT_solver.py
@@ -7,5 +7,32 @@ depending on the outputs of logic tools, change the variables in miter circuit
 iterate the code unitl no dip
 insert the variables when the dip is false to get keys'''
 
-with open('netlist.txt', 'w') as f:
-    f.write('readme')
+
+with open("C:\\Users\\sxc210186\\Documents\\GitHub\\CSAW\\SAT_solver\\netlist.txt", 'r') as f:
+    lines = f.readlines()
+    f='='
+    result=[]
+    i=0
+    t='nand'
+    l=len(t)+1
+    for line in lines:
+        string1=line
+
+        
+        part = string1.partition('=')
+        result.append(part)
+    print(result)
+    '''for i in range(0,len(part)):
+         if(t in part[i][0:l]):
+                   print(line)'''
+       
+
+
+
+            
+
+
+
+
+
+
